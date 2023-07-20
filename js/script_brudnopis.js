5a99ce6feb175e8b8ef9c5a2ec83b8f957449792
Bartosz Malolepszy <bartosz.malolepszy@interia.com>
2023-07-20 23:41:29 +0200

    6.3 add class active to the correct article

--- js/script_brudnopis.js
+++ js/script_brudnopis.js
@@ -1,23 +1,17 @@
 'use strict';
 const titleClickHandler = function (event) {
   console.log('Link was clicked!');
-  event.preventDefault();
-
-  /* remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
+  console.log('activeLinks', activeLinks);
 
   for (let activeLink of activeLinks) {
     activeLink.classList.remove('active');
   }
 
-  /* add class 'active' to the clicked link */
   const clickedElement = this;
-  //console.log('clickedElement:', clickedElement);
   console.log('clickedElement (with plus): ' + clickedElement);
 
   clickedElement.classList.add('active');
-
-  /* remove class 'active' from all articles */
 
   const acitveArticles = document.querySelectorAll('.active');
 
@@ -25,14 +19,11 @@ const titleClickHandler = function (event) {
     acitveArticle.classList.remove('active');
   }
 
-  /* get 'href' attribute from the clicked link */
   const articleSelector = clickedElement.getAttribute('href');
   console.log(articleSelector);
 
-  /* find the correct article using the selector (value of 'href' attribute) */
   const targetArticle = document.querySelector(articleSelector);
 
-  /* add class 'active' to the correct article */
   targetArticle.classList.add('active');
 };
 const links = document.querySelectorAll('.titles a');
